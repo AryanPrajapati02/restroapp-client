8049cd13e6e8d288e84fea2f78ccacd27d443271
Aryan <aryanprajapati9667@gmail.com>
2024-07-02 18:15:35 +0530

    ..

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://restroapp-server-5.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
